1ebc47f7e6e7c9ff270db2cd201f6883027c0435
ncsereoka <39672261+ncsereoka@users.noreply.github.com>
2020-10-01 20:14:17 +0300

    Add prop file to enable gui-independent running

--- src/main/java/upt/cti/svv/app/Svvitch.java
+++ src/main/java/upt/cti/svv/app/Svvitch.java
@@ -2,13 +2,20 @@ package upt.cti.svv.app;
 
 import upt.cti.svv.gui.SvvitchInterface;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public final class Svvitch {
 	private static final SvvitchInterface gui = new SvvitchInterface();
 	private static ApplicationState state = ApplicationState.STOPPED;
 
 	public static void main(String[] args) {
-		gui.display();
-//		new HttpWebServer().listen();
+		if (runSilently()) {
+//			new HttpWebServer().listen();
+		} else {
+			gui.display();
+		}
 	}
 
 	public static void updateGui() {
@@ -21,5 +28,18 @@ public final class Svvitch {
 
 	public static ApplicationState getState() {
 		return state;
+	}
+
+	private static boolean runSilently() {
+		try (InputStream in = Svvitch.class.getClassLoader().getResourceAsStream("application.properties");) {
+			Properties prop = new Properties();
+			if (in != null) {
+				prop.load(in);
+			}
+			final String guiString = prop.getProperty("silent");
+			return guiString != null && !guiString.equals("");
+		} catch (IOException e) {
+			return false;
+		}
 	}
 }
