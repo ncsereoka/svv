be75d0a26037480394ca60656b9b2f859a364554
ncsereoka <39672261+ncsereoka@users.noreply.github.com>
2020-10-10 19:59:15 +0300

    Simplify test

--- src/test/java/upt/cti/svv/HttpRequestParserTest.java
+++ src/test/java/upt/cti/svv/HttpRequestParserTest.java
@@ -8,91 +8,58 @@ import java.io.BufferedReader;
 import java.io.StringReader;
 
 public class HttpRequestParserTest {
+	private static final String VALID_GET_TO_ROOT = "GET / HTTP/1.1\n";
+	private static final String VALID_TEST_HEADERS = "Host: localhost:3000\n" +
+			"User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0\n" +
+			"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n" +
+			"Accept-Language: en-US,en;q=0.5\n" +
+			"Accept-Encoding: gzip, deflate\n" +
+			"DNT: 1\n" +
+			"Connection: keep-alive\n" +
+			"Upgrade-Insecure-Requests: 1\n" +
+			"Cache-Control: max-age=0\n";
+
 	@Test
 	public void valid_request() {
-		HttpRequestParser.from(toBufferedReader(validGetToRoot()));
+		parse(VALID_GET_TO_ROOT + VALID_TEST_HEADERS);
 	}
 
 	@Test(expected = InvalidRequestException.class)
 	public void unknown_method() {
-		HttpRequestParser.from(toBufferedReader(validPostToRoot()));
+		parse("POST / HTTP/1.1\n" + VALID_TEST_HEADERS);
 	}
 
 	@Test(expected = InvalidRequestException.class)
 	public void invalid_request_line_missing_url() {
-		HttpRequestParser.from(toBufferedReader(invalidGetToRootMissingUrl()));
+		parse("GET HTTP/1.1\n" + VALID_TEST_HEADERS);
 	}
 
 	@Test(expected = InvalidRequestException.class)
 	public void invalid_request_line_invalid_url() {
-		HttpRequestParser.from(toBufferedReader(invalidGetToRootInvalidUrl()));
+		parse("GET home HTTP/1.1\n" + VALID_TEST_HEADERS);
 	}
 
 	@Test(expected = InvalidRequestException.class)
 	public void malformed_header() {
-		HttpRequestParser.from(toBufferedReader(validGetToRootWithMalformedHeader()));
+		parse(VALID_GET_TO_ROOT + VALID_TEST_HEADERS + "Referer\n");
 	}
 
 	@Test(expected = InvalidRequestException.class)
 	public void invalid_header() {
-		HttpRequestParser.from(toBufferedReader(validGetToRootWithInvalidHeaderFormat()));
+		parse(VALID_GET_TO_ROOT + VALID_TEST_HEADERS + "Referer localhost:3000\n");
 	}
 
 	@Test(expected = InvalidRequestException.class)
 	public void invalid_http_version() {
-		HttpRequestParser.from(toBufferedReader(validGetToRootWithInvalidHttpVersion()));
+		parse("GET / HTTP/1.2\n" + VALID_TEST_HEADERS);
 	}
 
 	@Test(expected = InvalidRequestException.class)
 	public void incomplete_header() {
-		HttpRequestParser.from(toBufferedReader(validGetToRootWithIncompleteHeader()));
+		parse(VALID_GET_TO_ROOT + VALID_TEST_HEADERS + "Referer:\n");
 	}
 
-	private BufferedReader toBufferedReader(String text) {
-		return new BufferedReader(new StringReader(text));
-	}
-
-	private String validGetToRoot() {
-		return "GET / HTTP/1.1\n" + testHeaders();
-	}
-
-	private String validGetToRootWithInvalidHttpVersion() {
-		return "GET / HTTP/1.2\n" + testHeaders();
-	}
-
-	private String validPostToRoot() {
-		return "POST / HTTP/1.1\n" + testHeaders();
-	}
-
-	private String invalidGetToRootMissingUrl() {
-		return "GET HTTP/1.1\n" + testHeaders();
-	}
-
-	private String invalidGetToRootInvalidUrl() {
-		return "GET home HTTP/1.1\n" + testHeaders();
-	}
-
-	private String validGetToRootWithInvalidHeaderFormat() {
-		return "GET / HTTP/1.1\n" + testHeaders() + "Referer localhost:3000\n";
-	}
-
-	private String validGetToRootWithIncompleteHeader() {
-		return "GET / HTTP/1.1\n" + testHeaders() + "Referer:\n";
-	}
-
-	private String validGetToRootWithMalformedHeader() {
-		return "GET / HTTP/1.1\n" + testHeaders() + "Referer\n";
-	}
-
-	private String testHeaders() {
-		return "Host: localhost:3000\n" +
-				"User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0\n" +
-				"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n" +
-				"Accept-Language: en-US,en;q=0.5\n" +
-				"Accept-Encoding: gzip, deflate\n" +
-				"DNT: 1\n" +
-				"Connection: keep-alive\n" +
-				"Upgrade-Insecure-Requests: 1\n" +
-				"Cache-Control: max-age=0\n";
+	private void parse(String text) {
+		HttpRequestParser.from(new BufferedReader(new StringReader(text)));
 	}
 }
